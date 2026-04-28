137871294442384d9f9bf3cb5b68cd591ecb895c
JIA JIN HONG <jiajinh730@gmail.com>
2026-04-28 11:12:23 +0800

    更改個人介紹

---
+++ js/myjs.js
@@ -0,0 +1,5 @@
+var name = "Corgi.";
+var age = 18;
+
+console.log("I'm " + name+"I'm "+age+".I want to go home,just let me pass the final exam.");
+console.log("Why the hell is this class so fucking hard...")
